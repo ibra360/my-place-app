ae6eaa3b8f06b36fd8c3de4e6a0a9705fe79d01d
Abdullah Ibrahim <a.ebrahim360@outlook.com>
2020-06-07 15:01:31 -0700

    Version 1.0.0

---
+++ app.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://abd:abd123@bismillah-f5fdy.mongodb.net/theft?retryWrites=true&w=majority",
+    { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("MongoDB Connected..");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000; //hosting purpose..
+app.listen(port, () => console.log(`Server Started on port ${port}`));
